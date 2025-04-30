4e18c8acd83e114214b9ec91c8a5c6205acd93ad
agarg0627 <garg.abhinav0627@gmail.com>
2025-04-29 20:20:43 -0700

    Added teachers

--- app.py
+++ app.py
@@ -1,18 +1,33 @@
 from flask import Flask, render_template, request, jsonify
-from dijkstra import classrooms, distances, coordinates, dijkstra
+from dijkstra import classrooms, distances, coordinates, teachers, dijkstra
 import json
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', rooms=classrooms)
+    return render_template('index.html', rooms=classrooms, teachers=teachers.keys())
 
 @app.route('/get_path', methods=['POST'])
 def get_path():
     data = request.get_json()
+    
     start = data.get('start')
     end = data.get('end')
+    start_type = data.get('start_type', 'room')  
+    end_type = data.get('end_type', 'room')      
+    
+    if start_type == 'teacher':
+        if start in teachers:
+            start = teachers[start]
+        else:
+            return jsonify({'error': f'No room found for teacher: {start}'})
+    
+    if end_type == 'teacher':
+        if end in teachers:
+            end = teachers[end]
+        else:
+            return jsonify({'error': f'No room found for teacher: {end}'})
     
     if start not in classrooms or end not in classrooms:
         return jsonify({'error': 'Invalid room name'})
@@ -28,7 +43,7 @@ def get_path():
                     'name': room,
                     'coords': coordinates[room]
                 })
-        
+                
         return jsonify({
             'distance': distance,
             'path': path,
